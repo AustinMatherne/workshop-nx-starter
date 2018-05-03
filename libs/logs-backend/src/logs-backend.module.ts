1e43fddb161b5be7330351bc84fa173ef2db01c0
Austin M. Matherne <AustinMatherne@ProtonMail.ch>
2018-05-03 09:21:05 -0500

    lab 1.3.5

--- libs/logs-backend/src/logs-backend.module.ts
+++ libs/logs-backend/src/logs-backend.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { LogService } from './log.service';
 
 @NgModule({
-  imports: [CommonModule, HttpClientModule]
+  imports: [CommonModule, HttpClientModule],
+  providers: [LogService]
 })
 export class LogsBackendModule {}
