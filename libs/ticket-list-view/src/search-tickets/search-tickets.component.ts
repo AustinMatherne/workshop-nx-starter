5e9882a6f5b7fa2d8939b7280c7d8cecc0d8e830
Austin M. Matherne <AustinMatherne@ProtonMail.ch>
2018-05-03 10:24:48 -0500

    lab 2.1.1

--- libs/ticket-list-view/src/search-tickets/search-tickets.component.ts
+++ libs/ticket-list-view/src/search-tickets/search-tickets.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
 import { TicketService, UserService } from '@tuskdesk-suite/backend';
 import { User } from '@tuskdesk-suite/data-models';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search-tickets',
@@ -13,11 +15,18 @@ export class SearchTicketsComponent implements OnInit {
   searchTerm = new FormControl();
   assignedToUser = new FormControl();
   searchResults$: Observable<SearchResult[]>;
-  users: User[];
+  users: User[] = [];
+  subscription: Subscription;
 
   constructor(private ticketService: TicketService, private userService: UserService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.subscription = this.assignedToUser.valueChanges
+      .pipe(switchMap(val => this.userService.users(val)))
+      .subscribe(users => {
+        this.users = users;
+      });
+  }
 
   setAssignedToUser(value) {
     this.assignedToUser.patchValue(value, { emitEvent: false });
