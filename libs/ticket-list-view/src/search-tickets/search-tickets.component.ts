8f93d7c0763ac4f9abbb4034c98187b3424e7924
Austin M. Matherne <AustinMatherne@ProtonMail.ch>
2018-05-03 10:25:52 -0500

    lab 2.1.2

--- libs/ticket-list-view/src/search-tickets/search-tickets.component.ts
+++ libs/ticket-list-view/src/search-tickets/search-tickets.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
+import { switchMap } from 'rxjs/operators';
 import { Subscription } from 'rxjs/Subscription';
 import { TicketService, UserService } from '@tuskdesk-suite/backend';
 import { User } from '@tuskdesk-suite/data-models';
-import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search-tickets',
   templateUrl: './search-tickets.component.html',
   styleUrls: ['./search-tickets.component.scss']
 })
-export class SearchTicketsComponent implements OnInit {
+export class SearchTicketsComponent implements OnInit, OnDestroy {
   searchTerm = new FormControl();
   assignedToUser = new FormControl();
   searchResults$: Observable<SearchResult[]>;
@@ -26,6 +26,10 @@ export class SearchTicketsComponent implements OnInit {
       .subscribe(users => {
         this.users = users;
       });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   setAssignedToUser(value) {
