549aeca2dac389e1adf2808e3356a8cc7ca43311
Austin M. Matherne <AustinMatherne@ProtonMail.ch>
2018-05-03 10:57:54 -0500

    lab 2.2.3

--- libs/ticket-list-view/src/search-tickets/search-tickets.component.ts
+++ libs/ticket-list-view/src/search-tickets/search-tickets.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
-import { switchMap } from 'rxjs/operators';
+import { of } from 'rxjs/observable/of';
+import { switchMap, filter, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { Subscription } from 'rxjs/Subscription';
 import { TicketService, UserService } from '@tuskdesk-suite/backend';
 import { User } from '@tuskdesk-suite/data-models';
@@ -15,12 +16,17 @@ export class SearchTicketsComponent implements OnInit {
   searchTerm = new FormControl();
   assignedToUser = new FormControl();
   searchResults$: Observable<SearchResult[]>;
-  users: Observable<User[]>;
+  users$: Observable<User[]>;
 
   constructor(private ticketService: TicketService, private userService: UserService) {}
 
   ngOnInit() {
-    this.users = this.assignedToUser.valueChanges.pipe(switchMap(val => this.userService.users(val)));
+    this.users$ = this.assignedToUser.valueChanges.pipe(
+      filter((name: string) => !!name.length),
+      debounceTime(500),
+      distinctUntilChanged(),
+      switchMap(name => (name.length > 0 ? this.userService.users(name) : of(null)))
+    );
   }
 
   setAssignedToUser(value) {
